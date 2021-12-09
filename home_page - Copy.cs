2abeeadd1b9aedfda1f7f273c863ea1935c7e610
Divya Mariyam Eldo <61587940+Divya-mariyam@users.noreply.github.com>
2021-12-09 17:46:06 +0530

    Home page of Ayudar

---
+++ home_page - Copy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class home_page : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void parent()
+    {
+        SceneManager.LoadScene("login");
+
+    }
+    public void doctor()
+    {
+        SceneManager.LoadScene("dr_login");
+    }
+    public void quitapp()
+  {
+  Debug.Log("App Closed");
+  Application.Quit();
+  }
+}
