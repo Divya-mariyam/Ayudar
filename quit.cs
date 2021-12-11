b60614d9194486bbca7c29b495f6c8ccdc3b7aba
SABIRA NAZAR <sabiranazar591@gmail.com>
2021-12-11 17:55:25 +0530

    Exiting from the application

---
+++ quit.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class quit : MonoBehaviour
+{
+public void quitapp()
+  {
+  Debug.Log("App Closed");
+  Application.Quit();
+  }
+}
