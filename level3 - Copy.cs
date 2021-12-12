cc5838e8d558815d64b9fc4c3aa873120c1a1d85
FathimaNejiya <85240114+FathimaNejiya@users.noreply.github.com>
2021-12-12 01:57:15 -0800

    commited successfully

---
+++ level3 - Copy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;                                       
+using UnityEngine;
+
+public class level3 : MonoBehaviour
+{ AudioSource myaudio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+myaudio=GetComponent<AudioSource>();
+//myaudio.clip=Resources.Load<AudioClip>("BELL");
+myaudio.Play();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
